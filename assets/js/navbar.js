013630d7270df7eb0ddc2a6ffcc318a96939e276
rprime23 <reynamurphy23@gmail.com>
2026-01-30 13:51:12 -0700

    mobile performance increase after navbar setup

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,7 @@
-fetch("/assets/page_elements/navbar.html")
-  .then(response => response.text())
-  .then(html => {
-    document.getElementById("navbar-placeholder").innerHTML = html;
-});   
+fetch("assets/page_elements/navbar.html", { cache: "force-cache" })
+  .then((r) => r.text())
+  .then((html) => {
+    const mount = document.getElementById("navbar-placeholder");
+    if (mount) mount.innerHTML = html;
+  })
+  .catch(console.error);
